c653fe95181790b473a0335b3e62aff5e9f9b247
Wouter Gritter <woutergritter@gmail.com>
2023-12-09 22:54:06 +0100

    Day 9 additions

--- src/main/java/me/gritter/aoc2023/Day9.java
+++ src/main/java/me/gritter/aoc2023/Day9.java
@@ -3,6 +3,7 @@ package me.gritter.aoc2023;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -35,27 +36,63 @@ public class Day9 {
         private final List<Integer> numbers;
 
         public Sequence(List<Integer> numbers) {
-            if (numbers.isEmpty()) {
-                throw new IllegalStateException();
+            if (numbers.stream().anyMatch(Objects::isNull)) {
+                throw new NullPointerException();
             }
 
             this.numbers = numbers;
+        }
+
+        public Sequence(Integer... numbers) {
+            this(List.of(numbers));
         }
 
         public List<Integer> getNumbers() {
             return Collections.unmodifiableList(numbers);
         }
 
+        public int size() {
+            return numbers.size();
+        }
+
         public int getLast() {
+            if (numbers.isEmpty()) {
+                throw new IllegalStateException();
+            }
+
             return numbers.get(numbers.size() - 1);
         }
 
         public int getFirst() {
+            if (numbers.isEmpty()) {
+                throw new IllegalStateException();
+            }
+
             return numbers.get(0);
         }
 
+        public Sequence grow() {
+            return grow(1);
+        }
+
+        public Sequence grow(int steps) {
+            if (steps == 0) {
+                return this;
+            }
+
+            int previous = predictPrevious();
+            int next = predictNext();
+
+            List<Integer> before = new ArrayList<>();
+            before.add(previous);
+            before.addAll(numbers);
+            before.add(next);
+
+            return new Sequence(before).grow(steps - 1);
+        }
+
         public int predictNext() {
-            if (isZeros()) {
+            if (numbers.isEmpty() || isZeros()) {
                 return 0;
             }
 
@@ -64,7 +101,7 @@ public class Day9 {
         }
 
         public int predictPrevious() {
-            if (isZeros()) {
+            if (numbers.isEmpty() || isZeros()) {
                 return 0;
             }
 
@@ -91,7 +128,102 @@ public class Day9 {
 
         @Override
         public String toString() {
-            return numbers.toString();
+            int longestNumber = findLongestNumberRecursively();
+            int charsPerNumber = longestNumber % 2 == 1 ? longestNumber : longestNumber + 1;
+            int omitLeftPaddingNumber = padCalculateLeftPadding(String.valueOf(numbers.get(0)), charsPerNumber);
+
+            StringBuilder builder = new StringBuilder();
+            buildToStringRecursively(builder, charsPerNumber, omitLeftPaddingNumber, 0);
+            return builder.toString();
+        }
+
+        private int findLongestNumberRecursively() {
+            if (numbers.isEmpty()) {
+                return 0;
+            }
+
+            int longest = 0;
+            for (int n : numbers) {
+                String s = String.valueOf(n);
+                longest = Math.max(longest, s.length());
+            }
+
+            return Math.max(longest, calculateDeltas().findLongestNumberRecursively());
+        }
+
+        private void buildToStringRecursively(StringBuilder builder, int charsPerNumber, int omitLeftPaddingNumber, int paddingLeft) {
+            int padding = paddingLeft * (charsPerNumber / 2 + 1) - omitLeftPaddingNumber;
+            for (int i = 0; i < padding; i++) {
+                builder.append(' ');
+            }
+
+            for (int i = 0; i < numbers.size(); i++) {
+                boolean isFirst = i == 0;
+                boolean isLast = i == numbers.size() - 1;
+
+                String s = String.valueOf(numbers.get(i));
+                appendPadded(builder, s, charsPerNumber, isFirst ? -padding : 0, isLast ? Integer.MAX_VALUE : 0);
+
+                if (!isLast) {
+                    builder.append(' ');
+                }
+            }
+
+            if (!isZeros()) {
+                Sequence deltas = calculateDeltas();
+                if (deltas.size() > 0) {
+                    builder.append('\n');
+                    deltas.buildToStringRecursively(builder, charsPerNumber, omitLeftPaddingNumber, paddingLeft + 1);
+                }
+            }
+        }
+
+        private static int padCalculateLeftPadding(String s, int maxLength) {
+            boolean left = false;
+            int length = s.length();
+
+            int leftPad = 0;
+            while (length < maxLength) {
+                if (left) {
+                    leftPad++;
+                }
+
+                left = !left;
+                length++;
+            }
+
+            return leftPad;
+        }
+
+        private static void appendPadded(StringBuilder builder, String s, int maxLength, int omitLeft, int omitRight) {
+            boolean left = false;
+            int length = s.length();
+
+            int leftPad = 0;
+            int rightPad = 0;
+
+            while (length < maxLength) {
+                if (left && omitLeft-- <= 0) {
+                    leftPad++;
+                }
+
+                if (!left && omitRight-- <= 0) {
+                    rightPad++;
+                }
+
+                left = !left;
+                length++;
+            }
+
+            for (int i = 0; i < leftPad; i++) {
+                builder.append(' ');
+            }
+
+            builder.append(s);
+
+            for (int i = 0; i < rightPad; i++) {
+                builder.append(' ');
+            }
         }
     }
 }
